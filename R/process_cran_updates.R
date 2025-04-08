22ba1970c3dc3d0d2649a59d788c85afe60c839e
pat-s <patrick.schratz@gmail.com>
2025-04-08 20:18:03 +0200

    fix: process 'removed' packages before 'new' packages

--- R/process_cran_updates.R
+++ R/process_cran_updates.R
@@ -79,43 +79,6 @@ process_cran_updates <- function(
   if (is.null(s3_bucket)) {
     stop("s3_bucket must be defined")
   }
-  # Get list of updated and new packages for a specific day
-  updated_pkgs <- get_updated_cran_packages(interval)
-  new_pkgs <- get_new_cran_packages(interval)
-
-  all_pkgs <- dplyr::bind_rows(updated_pkgs, new_pkgs)
-
-  if (any(c(process_updated, process_new))) {
-    cli::cli_par()
-    cli::cli_end()
-
-    cli::cli_alert_success("{.fun process_cran_updates}: Updated packages:")
-    print(updated_pkgs)
-
-    cli::cli_par()
-    cli::cli_end()
-    cli::cli_alert_success("{.fun process_cran_updates}: New packages:")
-    print(new_pkgs)
-
-    # make R CMD Check happy
-    OS_type <- NULL
-    Package <- NULL
-    name <- NULL
-
-    `%nin%` <- Negate(`%in%`)
-    win_only <- withr::with_options(list(
-      repos = structure(c(CRAN = "https://cloud.r-project.org"))
-    ), tools::CRAN_package_db()) |>
-      filter(`OS_type` == "windows") |>
-      pull(Package)
-
-    all_pkgs <- all_pkgs |>
-      filter(`name` %nin% win_only)
-
-    purrr::walk2(all_pkgs$name, all_pkgs$version, ~ {
-      build_binary_package(.x, .y, platform = platform, upload = upload, archive = archive, force = force, store_build_metadata = store_build_metadata, s3_endpoint = s3_endpoint, s3_bucket = s3_bucket, s3_region = s3_region, s3_access_key_id = s3_access_key_id, s3_secret_access_key = s3_secret_access_key, metadata_db_type = metadata_db_type, metadata_db_host = metadata_db_host, metadata_db_name = metadata_db_name, metadata_db_table = metadata_db_table, metadata_db_port = metadata_db_port, metadata_db_user = metadata_db_user, metadata_db_password = metadata_db_password, metadata_db_sslmode = metadata_db_sslmode)
-    })
-  }
 
   if (process_removed) {
     removed_pkgs <- get_removed_cran_packages(interval)
@@ -159,4 +122,43 @@ process_cran_updates <- function(
       }
     })
   }
+  
+  # Get list of updated and new packages for a specific day
+  updated_pkgs <- get_updated_cran_packages(interval)
+  new_pkgs <- get_new_cran_packages(interval)
+
+  all_pkgs <- dplyr::bind_rows(updated_pkgs, new_pkgs)
+
+  if (any(c(process_updated, process_new))) {
+    cli::cli_par()
+    cli::cli_end()
+
+    cli::cli_alert_success("{.fun process_cran_updates}: Updated packages:")
+    print(updated_pkgs)
+
+    cli::cli_par()
+    cli::cli_end()
+    cli::cli_alert_success("{.fun process_cran_updates}: New packages:")
+    print(new_pkgs)
+
+    # make R CMD Check happy
+    OS_type <- NULL
+    Package <- NULL
+    name <- NULL
+
+    `%nin%` <- Negate(`%in%`)
+    win_only <- withr::with_options(list(
+      repos = structure(c(CRAN = "https://cloud.r-project.org"))
+    ), tools::CRAN_package_db()) |>
+      filter(`OS_type` == "windows") |>
+      pull(Package)
+
+    all_pkgs <- all_pkgs |>
+      filter(`name` %nin% win_only)
+
+    purrr::walk2(all_pkgs$name, all_pkgs$version, ~ {
+      build_binary_package(.x, .y, platform = platform, upload = upload, archive = archive, force = force, store_build_metadata = store_build_metadata, s3_endpoint = s3_endpoint, s3_bucket = s3_bucket, s3_region = s3_region, s3_access_key_id = s3_access_key_id, s3_secret_access_key = s3_secret_access_key, metadata_db_type = metadata_db_type, metadata_db_host = metadata_db_host, metadata_db_name = metadata_db_name, metadata_db_table = metadata_db_table, metadata_db_port = metadata_db_port, metadata_db_user = metadata_db_user, metadata_db_password = metadata_db_password, metadata_db_sslmode = metadata_db_sslmode)
+    })
+  }
+
 }
