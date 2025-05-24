7b8189cf975db7c48c2579ca95f7dc5343859cf7
pat-s <patrick.schratz@gmail.com>
2025-05-24 21:01:17 +0200

    fix: make R CMD Check happy

--- R/archive_packages.R
+++ R/archive_packages.R
@@ -20,21 +20,25 @@
 #' archive_package("AATtools", codename = "rhel9")
 #' archive_package("adw",
 #'   codename = "rhel8", arch = "amd64",
-#'   s3_endpoint = "https://hel1.your-objectstorage.com", s3_region = "hel1", s3_bucket = "devxy-r-package-binaries-hel1", s3_access_key_id = Sys.getenv("HETZNER_S3_ACCESS_KEY_K3S"), s3_secret_access_key = Sys.getenv("HETZNER_S3_SECRET_KEY_K3S")
+#'   s3_endpoint = "https://hel1.your-objectstorage.com", s3_region = "hel1",
+#'   s3_bucket = "devxy-r-package-binaries-hel1",
+#'   s3_access_key_id = Sys.getenv("HETZNER_S3_ACCESS_KEY_K3S"),
+#'   s3_secret_access_key = Sys.getenv("HETZNER_S3_SECRET_KEY_K3S")
 #' )
 #' }
 #'
 archive_package <- function(
-    package_name,
-    codename = NULL,
-    local_output_dir_root = ".",
-    s3_endpoint,
-    s3_region,
-    s3_bucket,
-    arch = NULL,
-    debug = FALSE,
-    s3_access_key_id = NULL,
-    s3_secret_access_key = NULL) {
+  package_name,
+  codename = NULL,
+  local_output_dir_root = ".",
+  s3_endpoint,
+  s3_region,
+  s3_bucket,
+  arch = NULL,
+  debug = FALSE,
+  s3_access_key_id = NULL,
+  s3_secret_access_key = NULL
+) {
   s3fs::s3_file_system(
     aws_access_key_id = s3_access_key_id,
     aws_secret_access_key = s3_secret_access_key,
@@ -59,7 +63,12 @@ archive_package <- function(
   }
 
   local_bin_dir <- set_bin_path(local_output_dir_root, codename)
-  remote_bin_dir <- sprintf("%s/%s/%s/latest/src/contrib", s3_bucket, arch, codename)
+  remote_bin_dir <- sprintf(
+    "%s/%s/%s/latest/src/contrib",
+    s3_bucket,
+    arch,
+    codename
+  )
 
   # suppress progressr output here
   progressr::handlers("void")
@@ -70,7 +79,9 @@ archive_package <- function(
 
   foo <- lapply(package_name, function(pkgname) {
     cli::cli_h2("Archiving ({.pkg {pkgname}})")
-    if (!s3fs::s3_dir_exists(sprintf("%s/Archive/%s", remote_bin_dir, pkgname))) {
+    if (
+      !s3fs::s3_dir_exists(sprintf("%s/Archive/%s", remote_bin_dir, pkgname))
+    ) {
       s3fs::s3_dir_create(sprintf("%s/Archive/%s", remote_bin_dir, pkgname))
     }
     all_versions <- grep(sprintf("/%s_", pkgname), files, value = TRUE)
@@ -78,47 +89,89 @@ archive_package <- function(
     if (length(all_versions) > 1) {
       # get most recent version from CRAN
 
-      last_version <- strsplit(gh::gh(sprintf("GET /repos/cran/%s/commits", package_name))[[1]]$commit$message, "version ")[[1]][2]
+      last_version <- strsplit(
+        gh::gh(sprintf("GET /repos/cran/%s/commits", package_name))[[
+          1
+        ]]$commit$message,
+        "version "
+      )[[1]][2]
 
       # check if last version is available in repo
-      if (any(grepl(sprintf("%s_%s.tar.gz", package_name, last_version), all_versions))) {
-        index <- which(grepl(sprintf("_%s.tar.gz", last_version), all_versions, fixed = TRUE))
+      if (
+        any(grepl(
+          sprintf("%s_%s.tar.gz", package_name, last_version),
+          all_versions
+        ))
+      ) {
+        index <- which(grepl(
+          sprintf("_%s.tar.gz", last_version),
+          all_versions,
+          fixed = TRUE
+        ))
         old_versions <- all_versions[-index]
       } else {
         # this often fails with
         # caused by error in `curl::curl_fetch_memory(url)`:
         # ! SSL peer certificate or SSH remote key was not OK: [crandb.r-pkg.org] SSL certificate problem: unable to get local issuer certificate
         versions <- insistently(
-          ~
-            rev(pak::pkg_history(pkgname)$Version),
-          rate = retry_config, quiet = FALSE
+          ~ rev(pak::pkg_history(pkgname)$Version),
+          rate = retry_config,
+          quiet = FALSE
         )()
         for (i in versions) {
-          if (any(grepl(paste0("^", i, "$"), stringr::str_split(stringr::str_split(all_versions, "_", simplify = T)[, 2], ".tar.gz", simplify = TRUE)[, 1]))) {
+          if (
+            any(grepl(
+              paste0("^", i, "$"),
+              stringr::str_split(
+                stringr::str_split(all_versions, "_", simplify = T)[, 2],
+                ".tar.gz",
+                simplify = TRUE
+              )[, 1]
+            ))
+          ) {
             index <- which(grepl(sprintf("_%s.tar.gz", i), all_versions))
             old_versions <- all_versions[-index]
             break
           }
         }
       }
-      cli::cli_alert("Archiving {.field {basename(old_versions)}}, keeping {.field {basename(all_versions[index])}}.")
+      cli::cli_alert(
+        "Archiving {.field {basename(old_versions)}}, keeping {.field {basename(all_versions[index])}}."
+      )
       # account for duplicated (= faulty) packages
       if (anyDuplicated(s3fs::s3_file_info(old_versions)$key) > 0) {
         for (i in old_versions) {
           if (anyDuplicated(s3fs::s3_file_info(i)$key)) {
             cli::cli_alert_danger("{.field {i}} is duplicated, deleting it.")
             s3fs::s3_file_delete(i)
-            ntfy::ntfy_send(sprintf("Package %s is duplicated and has been deleted, it must be rebuilt.", i))
+            ntfy::ntfy_send(sprintf(
+              "Package %s is duplicated and has been deleted, it must be rebuilt.",
+              i
+            ))
             old_versions <- setdiff(old_versions, i)
           }
         }
       }
       if (length(old_versions) > 0) {
-        s3fs::s3_file_move(old_versions, sprintf("%s/Archive/%s/%s", remote_bin_dir, pkgname, basename(old_versions)), max_batch = fs::fs_bytes("300MB"), overwrite = TRUE)
-        cli::cli_alert_success("Successfully archived package {.pkg {pkgname}}.")
+        s3fs::s3_file_move(
+          old_versions,
+          sprintf(
+            "%s/Archive/%s/%s",
+            remote_bin_dir,
+            pkgname,
+            basename(old_versions)
+          ),
+          max_batch = fs::fs_bytes("300MB"),
+          overwrite = TRUE
+        )
+        cli::cli_alert_success(
+          "Successfully archived package {.pkg {pkgname}}."
+        )
       }
     } else {
-      cli::cli_alert("Skipping {.pkg {pkgname}} as only one package versions exists.")
+      cli::cli_alert(
+        "Skipping {.pkg {pkgname}} as only one package versions exists."
+      )
     }
   })
 
@@ -143,13 +196,24 @@ write_archive_rds <- function(files) {
   dt <- dt[grepl("\\.tar\\.gz$", file_path)]
 
   # split into package and version
-  dt[, c("package", "version") := tstrsplit(sub("\\.tar\\.gz$", "", file_path), "_", fixed = TRUE)]
+  dt[,
+    c("package", "version") := tstrsplit(
+      sub("\\.tar\\.gz$", "", file_path),
+      "_",
+      fixed = TRUE
+    )
+  ]
 
   # assign DF row names
   dt[, row_name := paste0(package, "/", package, "_", version, ".tar.gz")]
 
   # Group by package and create a list of data.tables
-  result <- dt[, .(data_frame = list(as.data.table(setNames(list(row_name), c("row_name"))))), by = package]
+  result <- dt[,
+    .(
+      data_frame = list(as.data.table(setNames(list(row_name), c("row_name"))))
+    ),
+    by = package
+  ]
 
   # Convert each grouped data.table to a data.frame and assign row names
   result_list <- lapply(result$data_frame, function(dt_group) {
--- R/init-repo.R
+++ R/init-repo.R
@@ -1,7 +1,7 @@
 #' Initialize a new R repository
 #' @description
 #' Creates a new repository (S3 bucket) and checks connectivity to the metadata database
-#' 
+#'
 #' @template param-s3_endpoint
 #' @template param-s3_region
 #' @template param-s3_bucket
@@ -17,24 +17,35 @@
 #' @template param-metadata_db_password
 #' @template param-metadata_db_sslmode
 #'
+#' @importFrom utils menu
+#' @importFrom RSQLite SQLite
+#'
 #' @note
 #' When using SQLITE, make sure that the file is also available in the desired built environment, i.e. most likel in your CI/CD system. Usually it is easiest to store the file also in S3, as it can be reached there easily from within CI and local sources.
 #' @examples
-#' init_repo("https://hel1.your-objectstorage.com", "hel1", "devxy-r-package-binaries-hel1", Sys.getenv("HETZNER_S3_ACCESS_KEY_K3S"), Sys.getenv("HETZNER_S3_SECRET_KEY_K3S"),metadata_db_type = "postgres", metadata_db_host = 'r-binaries.devxy.io', metadata_db_name = 'build_metadata', metadata_db_table = 'deleteme', metadata_db_user = 'r_binaries', metadata_db_password = Sys.getenv('PGPASS'), metadata_db_sslmode = 'require', metadata_db_port = 15432)
+#' \dontrun{
+#' init_repo("https://hel1.your-objectstorage.com", "hel1", "devxy-r-package-binaries-hel1",
+#'   Sys.getenv("HETZNER_S3_ACCESS_KEY_K3S"), Sys.getenv("HETZNER_S3_SECRET_KEY_K3S"),
+#'   metadata_db_type = "postgres", metadata_db_host = 'r-binaries.devxy.io',
+#'   metadata_db_name = 'build_metadata', metadata_db_table = 'deleteme', 
+#'   metadata_db_user = 'r_binaries', metadata_db_password = Sys.getenv('PGPASS'), 
+#'   metadata_db_sslmode = 'require', 
+#'   metadata_db_port = 15432)}
 init_repo <- function(
-    s3_endpoint,
-    s3_region,
-    s3_bucket,
-    s3_access_key_id = NULL,
-    s3_secret_access_key = NULL,
-    metadata_db_type = "sqlite",
-    metadata_db_host = NULL,
-    metadata_db_name = NULL,
-    metadata_db_table = NULL,
-    metadata_db_port = NULL,
-    metadata_db_user = NULL,
-    metadata_db_password = NULL,
-    metadata_db_sslmode = NULL) {
+  s3_endpoint,
+  s3_region,
+  s3_bucket,
+  s3_access_key_id = NULL,
+  s3_secret_access_key = NULL,
+  metadata_db_type = "sqlite",
+  metadata_db_host = NULL,
+  metadata_db_name = NULL,
+  metadata_db_table = NULL,
+  metadata_db_port = NULL,
+  metadata_db_user = NULL,
+  metadata_db_password = NULL,
+  metadata_db_sslmode = NULL
+) {
   s3fs::s3_file_system(
     aws_access_key_id = s3_access_key_id,
     aws_secret_access_key = s3_secret_access_key,
@@ -53,27 +64,47 @@ init_repo <- function(
   if (metadata_db_type == "sqlite") {
     res = DBI::dbCanConnect(RSQLite::SQLite(), metadata_db_host)
   } else if (metadata_db_type == "postgres") {
-    res = DBI::dbCanConnect(RPostgres::Postgres(),
-      dbname = metadata_db_name, host = metadata_db_host,
-      port = metadata_db_port, user = metadata_db_user, password = metadata_db_password,
+    res = DBI::dbCanConnect(
+      RPostgres::Postgres(),
+      dbname = metadata_db_name,
+      host = metadata_db_host,
+      port = metadata_db_port,
+      user = metadata_db_user,
+      password = metadata_db_password,
       sslmode = metadata_db_sslmode
     )
     if (res) {
-      cli::cli_alert_success("Connectivity to DB {.field {metadata_db_host}}, database {.field {metadata_db_name}} established.")
+      cli::cli_alert_success(
+        "Connectivity to DB {.field {metadata_db_host}}, database {.field {metadata_db_name}} established."
+      )
     }
-    con = DBI::dbConnect(RPostgres::Postgres(), dbname = metadata_db_name, host = metadata_db_host,
-    port = metadata_db_port, user = metadata_db_user, password = metadata_db_password,
-    sslmode = metadata_db_sslmode)
+    con = DBI::dbConnect(
+      RPostgres::Postgres(),
+      dbname = metadata_db_name,
+      host = metadata_db_host,
+      port = metadata_db_port,
+      user = metadata_db_user,
+      password = metadata_db_password,
+      sslmode = metadata_db_sslmode
+    )
     exists_table = DBI::dbExistsTable(con, name = metadata_db_table)
     if (exists_table) {
-      cli::cli_alert_success("Table {.field {metadata_db_table}} exists. You're ready to build!")
-      cli::cli_alert_info("You can now use {.fun {build_binary_package}} with the provided S3 and DB settings to build and publish packages.")
+      cli::cli_alert_success(
+        "Table {.field {metadata_db_table}} exists. You're ready to build!"
+      )
+      cli::cli_alert_info(
+        "You can now use {.fun {build_binary_package}} with the provided S3 and DB settings to build and publish packages."
+      )
     } else {
-      cli::cli_alert_warning("Table {.field {metadata_db_table}} does not exist.")
-      answer = menu(c("Yes", "No"), title="Should it be created?")
+      cli::cli_alert_warning(
+        "Table {.field {metadata_db_table}} does not exist."
+      )
+      answer = menu(c("Yes", "No"), title = "Should it be created?")
       if (answer == 1) {
         DBI::dbCreateTable(con, metadata_db_table)
-        cli::cli_alert_success("Table {.field {metadata_db_table}} created. You're ready to build!")
+        cli::cli_alert_success(
+          "Table {.field {metadata_db_table}} created. You're ready to build!"
+        )
       }
     }
   }
