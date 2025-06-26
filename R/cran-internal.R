7e56b8d4984791c09b07d57bfa04f948c71837be
pat-s <patrick.schratz@gmail.com>
2025-06-27 00:14:58 +0200

    feat: add process_unarchived_pkgs()

--- R/cran-internal.R
+++ R/cran-internal.R
@@ -5,7 +5,7 @@
 get_missing_pkgs_db <- function(
   platform = "ubuntu-2204",
   arch = "amd64",
-  days_back = 2
+  days_back = 2L
 ) {
   con <- DBI::dbConnect(
     RPostgres::Postgres(),
@@ -53,4 +53,69 @@ get_missing_pkgs_db <- function(
   message("Number of missing packages: ", length(pkgs))
 
   invisible(pkgs)
+}
+
+#' Returns packages that need to be archived (have multiple versions)
+#' @param codename Operating system codename (e.g., "alpine322", "ubuntu-2204")
+#' @param arch Architecture (e.g., "arm64", "amd64")
+#' @param s3_endpoint S3 endpoint URL
+#' @param s3_region S3 region
+#' @param s3_bucket S3 bucket name
+#' @param s3_access_key_id S3 access key ID
+#' @param s3_secret_access_key S3 secret access key
+#' @param workers Number of workers to use for parallel processing
+#' @importFrom future plan
+#' @keywords internal
+#' @export
+process_unarchived_pkgs <- function(
+  codename,
+  arch,
+  s3_endpoint = "https://hel1.your-objectstorage.com",
+  s3_region = "hel1",
+  s3_bucket = "devxy-r-package-binaries-hel1",
+  s3_access_key_id = Sys.getenv("HETZNER_S3_ACCESS_KEY_K3S"),
+  s3_secret_access_key = Sys.getenv("HETZNER_S3_SECRET_KEY_K3S"),
+  workers = 1L
+) {
+  s3fs::s3_file_system(
+    aws_access_key_id = s3_access_key_id,
+    aws_secret_access_key = s3_secret_access_key,
+    endpoint = s3_endpoint,
+    region_name = s3_region,
+    refresh = TRUE
+  )
+
+  repo_path <- file.path(s3_bucket, arch, codename, "latest", "src", "contrib")
+  files <- s3fs::s3_dir_ls(repo_path)
+
+  package_names <- vapply(
+    strsplit(basename(files), "_", fixed = TRUE),
+    function(x) x[1L],
+    character(1L)
+  )
+
+  # Find packages with multiple versions (duplicated names)
+  duplicated_packages <- unique(package_names[duplicated(package_names)])
+  # Filter out Windows-only packages
+  non_archived <- setdiff(duplicated_packages, "RInno")
+  formatted_pkgs <- paste(shQuote(non_archived, type = "cmd"), collapse = " ")
+  cat(formatted_pkgs, "\n")
+  message("Number of packages needing archiving: ", length(non_archived))
+
+  if (length(non_archived) > 0L) {
+    future::plan("multisession", workers = workers)
+    out = future.apply::future_lapply(
+      non_archived,
+      archive_package,
+      codename = codename,
+      arch = arch,
+      s3_region = s3_region,
+      s3_endpoint = s3_endpoint,
+      s3_bucket = s3_bucket,
+      s3_access_key_id = s3_access_key_id,
+      s3_secret_access_key = s3_secret_access_key
+    )
+  }
+
+  invisible(non_archived)
 }
